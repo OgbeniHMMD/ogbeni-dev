73be6f6c071ea60f307a4105138b4a9a65a3e0ce
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-10-19 00:09:48 +0100

    v0.0.1

---
+++ components/ComingSoon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="
+      min-h-screen min-w-screen
+      h-screen
+      flex
+      items-center
+      justify-center
+      bg-light-300
+    "
+  >
+    <div class="text-xl font-bold text-green-800 p-4">
+      We are coming soon....
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtPage />
+    <coming-soon />
   </div>
 </template>
