7f21a97cc20c35b8881e2fc51ae35339db6812da
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-10-19 00:19:30 +0100

    Change colors to grayish

--- components/ComingSoon.vue
+++ components/ComingSoon.vue
@@ -8,9 +8,10 @@
       justify-center
       bg-light-300
     "
+    style="
+      background-image: url('https://www.transparenttextures.com/patterns/light-wool.png');
+    "
   >
-    <div class="text-xl font-bold text-green-800 p-4">
-      We are coming soon....
-    </div>
+    <div class="text-xl font-bold text-dark-800 p-4">We are coming soon...</div>
   </div>
 </template>
