acbc0b92553e2bd1fd40c3b945c18cfb13d82f61
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-10-22 08:05:47 +0100

    New Component: Coming soon

---
+++ components/ComingSoon.jsx
@@ -0,0 +1,11 @@
+export default function ComingSoon() {
+  return (
+    <main className="bg-light-100">
+      <div className="h-screen w-screen flex items-center justify-center">
+        <div className="font-bold text-dark-700 text-lg font-mono">
+          Coming Soon!
+        </div>
+      </div>
+    </main>
+  );
+}
