21697e7ff16a27f1536e418738818a7bde39cce0
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-10-26 23:58:14 +0100

    Logo update

--- components/_partials/NavigationBar.jsx
+++ components/_partials/NavigationBar.jsx
@@ -5,9 +5,13 @@ export default function NavigationBar() {
   return (
     <SectionContainer padding="sm" shade="dark">
       <section className="w-full flex justify-between items-center space-x-2">
-        <div className="font-medium font-mono text-3xl">&#128483;</div>
+        <Link href="/">
+          <a className="font-black font--mono text-4xl text-stroke-light-50 text-stroke-1 text-transparent">
+            <img src="/logo.png" className="h-8 lg:h-10" alt="logo" />
+          </a>
+        </Link>
 
-        <div className="uppercase flex space-x-4 font-medium tracking-wide">
+        <div className="hidden md:flex uppercase space-x-8 font-medium tracking-wide">
           <Link href="/#top">
             <a className="hover:(underline)">About</a>
           </Link>
