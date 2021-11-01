3d01401a71a5e21ff5f15cddf41af60c6b85f35c
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-11-01 13:11:04 +0100

    Contact API Integration

--- components/sections/home/HomeContactForm.jsx
+++ components/sections/home/HomeContactForm.jsx
@@ -1,15 +1,63 @@
 import SectionContainer from "../../SectionContainer";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useState } from "react";
 
 export default function HomeContactForm() {
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [response, setResponse] = useState({});
+
+  const onSubmit = async (data) => {
+    if (response.sending) return;
+
+    setResponse({
+      sending: true,
+      message: "Sending your message. Please, wait.",
+    });
+
+    await axios
+      .post("http://ogbeni.herokuapp.com/misc/contact", data)
+      .then((res) => {
+        reset();
+        setResponse({
+          success: true,
+          message: res?.data?.message,
+        });
+      })
+      .catch((e) => {
+        setResponse({
+          error: true,
+          message: e?.response?.data?.message,
+        });
+      })
+      .finally(() => {
+        setSending(false);
+      });
+  };
+
   return (
     <SectionContainer shade="white" padding="xl" id="contact">
       <div className="mx-auto max-w-screen-lg">
         <h1 className="font-medium text-center text-lg tracking-wide pb-8 uppercase">
           Get in touch
         </h1>
-        <form>
+
+        {(response.success || response.error) && (
+          <div className="border rounded mx-auto max-w-md border-l-4 border-dark-50 text-center mb-4 p-2 text-dark-50 md:p-4">
+            {response.message}
+          </div>
+        )}
+
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="pb-4 lg:pb-8">
             <input
+              {...register("fullName")}
               placeholder="Your Name"
               className="border rounded w-full p-4 "
             />
@@ -17,29 +65,39 @@ export default function HomeContactForm() {
           <section className="w-full md:flex md:space-x-4 lg:space-x-8">
             <div className="pb-4 md:w-1/2 lg:pb-8">
               <input
+                {...register("email")}
                 placeholder="E-mail"
                 className="border rounded w-full p-4"
               />
             </div>
             <div className="pb-4 md:w-1/2 lg:pb-8">
               <input
-                placeholder="Phone"
+                {...register("phone")}
+                placeholder="Phone (optional)"
                 className="border rounded w-full p-4"
               />
             </div>
           </section>
           <div className="pb-4">
             <textarea
+              {...register("text", { required: true })}
               placeholder="Enter your message here..."
               className="border rounded w-full min-h-40 p-4"
             />
+
+            {errors.text && (
+              <div className="text-xs text-dark-50 italic">
+                *Please, enter your message
+              </div>
+            )}
           </div>
           <div className="pb-4">
             <button
-              className="border rounded cursor-not-allowed text-sm py-4 px-6 hover:bg-light-50"
+              type="submit"
+              className="border rounded text-sm py-4 px-6 hover:bg-light-50"
               title="NOT IMPLEMENTED"
             >
-              Send Message
+              {response.sending ? "Sending Message..." : "Send Message"}
             </button>
           </div>
         </form>
