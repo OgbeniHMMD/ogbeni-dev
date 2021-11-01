b16d2c676a22ecd635633f1300b7edb302af7eee
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-11-01 11:58:21 +0100

    Added Twitter handle to footer

--- components/_partials/FooterCopyright.jsx
+++ components/_partials/FooterCopyright.jsx
@@ -3,12 +3,17 @@ import SectionContainer from "../SectionContainer";
 export default function FooterCopyRight() {
   return (
     <SectionContainer shade="light" padding="md">
-      <section className="text-dark-50 text-center">
+      <section className="text-center text-dark-50">
         <div className="tracking-tracking-wider pb-2">
-          Copyright &copy; 2021 -
-          <span className="font-medium"> OgbeniHMMD.</span>
+          Copyright &copy; 2021 {" - "}
+          <a
+            href="https://twitter.com/ogbenihmmd"
+            className="font-medium underline-dark-50 hover:underline"
+          >
+            @OgbeniHMMD
+          </a>
         </div>
-        <div className="text-sm font-mono">
+        <div className="font-mono text-sm">
           Proudly Nigerian &#127475;&#127468;
         </div>
       </section>
