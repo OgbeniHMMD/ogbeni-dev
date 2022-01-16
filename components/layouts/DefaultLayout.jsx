195aec24621996ec8cab1b78abf8f0a91cdab151
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-01-16 15:52:58 +0100

    Purged Unused Components

--- components/layouts/DefaultLayout.jsx
+++ components/layouts/DefaultLayout.jsx
@@ -1,7 +1,5 @@
 import Head from "next/head";
 import FooterCopyRight from "../_partials/FooterCopyright";
-import NavigationBar from "../_partials/NavigationBar";
-import TopSocialMediaLink from "../_partials/TopSocialMediaLink";
 
 export default function DefaultLayout(props) {
   return (
@@ -16,9 +14,6 @@ export default function DefaultLayout(props) {
       </Head>
 
       <main className="flex flex-col font-body min-h-screen bg-light-50 justify-between">
-        {/* <TopSocialMediaLink />
-        <NavigationBar /> */}
-
         <main className="flex-grow flex flex-col h-full">{props.children}</main>
         <FooterCopyRight />
       </main>
