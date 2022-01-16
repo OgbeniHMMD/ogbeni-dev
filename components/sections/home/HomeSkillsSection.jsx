592116b015341812eae230093f502b718d326e9c
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-01-16 15:14:34 +0100

    Home: Updated Skills Section

--- components/sections/home/HomeSkillsSection.jsx
+++ components/sections/home/HomeSkillsSection.jsx
@@ -8,7 +8,7 @@ export default function HomeSkillsSection() {
       url: "https://reactjs.org",
     },
     {
-      title: "Vue.js",
+      title: "Vue",
       icon: "/icons/vue.svg",
       url: "https://vuejs.org",
     },
@@ -22,21 +22,42 @@ export default function HomeSkillsSection() {
       icon: "/icons/nextjs.svg",
       url: "https://nextjs.org",
     },
+    {
+      title: "Expo",
+      icon: "/icons/expo.svg",
+      url: "https://expo.dev",
+    },
+    {
+      title: "TailwindCSS",
+      icon: "/icons/tailwindcss.svg",
+      url: "https://tailwindcss.com",
+    },
+    {
+      title: "Figma",
+      icon: "/icons/figma.svg",
+      url: "https://figma.com",
+    },
   ];
 
   return (
     <SectionContainer padding="lg" shade="white" id="skills">
-      <h1 className="font-medium text-center text-lg tracking-wide pb-8 uppercase">
+      <h1 className="font-medium text-center text-lg tracking-wide pb-8 uppercase lg:pb-16">
         Skills &amp; Tools
       </h1>
-      <div className="flex flex-wrap space-x-8 space-y-4 justify-center items-end md:space-y-0">
+      <div className="flex flex-wrap gap-8 justify-center items-end md:space-y-0">
         {tools.map((tool) => (
-          <a href={tool.url} key={tool.title} target="_blank" rel="noreferrer">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
+          <a
+            href={tool.url}
+            key={tool.title}
+            title={tool.title}
+            target="_blank"
+            rel="noreferrer"
+            className="p-2 group"
+          >
             <img
               key={tool.title}
               src={tool.icon}
-              className="h-16 w-auto"
+              className="mx-auto h-16 w-auto transform z-10 duration-200 group-hover:scale-110"
               alt={tool.title}
             />
           </a>
